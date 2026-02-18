49606c03980b14d051f9920245f0d3e1bd7cf4e0
rohit <rohitdeppi38@gmail.com>
2026-02-18 12:09:08 +0530

    adding backend

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,14 @@
-console.log("server started");
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    console.log(req.body);
+    res.send("Hello from typescript server")
+})
+
+app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
